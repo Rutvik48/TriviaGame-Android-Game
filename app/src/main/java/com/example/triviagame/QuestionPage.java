847b310615fd02627baff3973aa5f842cc7d3ad8
Rutvik Patel <rutvik48@gmail.com>
2019-05-12 11:11:24 -0400

    Finished game 1.2 Database has been updated.
    Signed-off-by: Rutvik Patel <rutvik48@gmail.com>

--- app/src/main/java/com/example/triviagame/QuestionPage.java
+++ app/src/main/java/com/example/triviagame/QuestionPage.java
@@ -3,6 +3,7 @@ package com.example.triviagame;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
